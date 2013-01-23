e996f5a7413aadaf9d86f01d8ab4b761ebeeb27e
Alexander Nilsson <a@alexandernilsson.com>
2013-01-23 15:07:50 +0100

    Revert "Simplify install and remove deps to yum puppet module"
    This reverts commit 74510afbe9ee1a619b31c73f700ace105ed5ff9d.

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,22 @@
 #
 class mongodb::params {
 
+	$repo_class = $operatingsystem ? {
+		/(?i)(Redhat|CentOS)/ => 'yum::repo::mongodb',
+	}
+
+	$server_pkg_name = $operatingsystem ? {
+		/(?i)(Debian|Ubuntu)/ => 'mongodb-10gen',
+		/(?i)(Redhat|CentOS)/ => 'mongo-10gen-server',
+		default               => undef,
+	}
+
+	$old_server_pkg_name = $operatingsystem ? {
+		/(?i)(Debian|Ubuntu)/ => 'mongodb-stable',
+		/(?i)(Redhat|CentOS)/ => 'mongodb-server',
+		default               => undef,
+	}
+
 	$run_as_user = 'mongod'
 	$run_as_group = 'mongod'
 
